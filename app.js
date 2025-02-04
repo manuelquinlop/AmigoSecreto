a34f62fc43409f4e4abf6997f81459178a97a1fa
manuelquinlop <manuel.quin.lop@gmail.com>
2025-02-03 23:39:08 -0300

    función para agregar amigos app.js
    función que permite al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    
+    listaAmigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
